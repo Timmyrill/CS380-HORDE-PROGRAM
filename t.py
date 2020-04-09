d4e059dd4464674ccf6e5ff6e1cc183ccf3e12cd
Timmyrill <wilsowil000@gmail.com>
2020-04-09 11:18:38 -0400

    This version is slick

--- t.py
+++ t.py
@@ -2,21 +2,9 @@ import random
 
 class Horde_Maker():
 	def __init__(self):
-		self.C = 0
 		self.B = 50
-		self.B1 = 0
-		self.B2 = 0
-		self.B3 = 0
-		self.B4 = 0
 		self.range_list = []
 		self.H = 0
-		self.c_loot = ["Copper Ring", "Polished Silverware", "Dagger", "Sword", "Spear", "Shield"]
-		self.c_val = {"Copper Ring": 8, "Polished Silverware": 20, "Dagger": 2, "Sword": 15, "Spear": 1, "Shield": 10, "Simple Amulet": 5}
-		self.b_loot = []
-		self.b1_loot = []
-		self.b2_loot = []
-		self.b3_loot = []
-		self.b4_loot = []
 		
 	def gen_bpower(self):
 		n = 0
@@ -27,33 +15,34 @@ class Horde_Maker():
 			except ValueError:
 				print("Invalid Input. ")
 				n = 0
-
-		self.C = self.B/(n*2)
-		self.B1 = self.B * (n*2)
-		self.B2 = self.B * (n*8)
-		self.B3 = self.B * (n*32)
-		self.B4 = self.B * (n*256) 
-		self.range_list = [self.C, self.C, self.C, self.C, self.B, self.B, self.B, self.B, self.B1, self.B1, self.B1, self.B1, self.B2, self.B2, self.B2, self.B2, self.B3, self.B3, self.B3, self.B3, self.B4]
-
+		
+		self.c = self.Power(self.B/(n*2), ["Copper Ring", "Polished Silverware", "Dagger", "Sword", "Spear", "Shield"], {"Copper Ring": 8, "Polished Silverware": 20, "Dagger": 2, "Sword": 15, "Spear": 1, "Shield": 10, "Simple Amulet": 5})
+		self.b = self.Power(self.B, ["Weak Magic Ring", "Weak Magic Bracer", "Ornate Necklace", "Potion of Healing", "Potion of Slickness"], {"Weak Magic Ring": 40, "Weak Magic Bracer": 35, "Ornate Necklace":75, "Potion of Healing": 50, "Potion of Slickness": 35})
+		self.b1 = self.Power(self.B * (n*2), ["Potion of Greater Healing", "Masterwork Weapon", "Drow Poison", "Rare Fang"], {"Potion of Greater Healing": 200, "Masterwork Weapon" : 300, "Drow Poison": 175, "Rare Fang": 100})
+		self.b2 = self.Power(self.B * (n*8), ["Potion of Superior Healing", "+1 Weapon"], {"Potion of Superior Healing": 800, "+1 Weapon": 2000})
+		self.b3 = self.Power(self.B * (n*32), ["Potion of Supreme Healing", "+2 Weapon"], {"Potion of Supreme Healing": 3200, "+2 Weapon": 8000})
+		self.b4 = self.Power(self.B * (n*256), ["Ambrosia", "Scroll of One-Thousand Swords", "Grimoir of Nemesis", "Ashen Staff", "The Hero's Sword", "+3 Weapon"], {"Ambrosia": 12000, "Scroll of One-Thousand Swords": 8000, "Grimoir of Nemesis": 10000, "Ashen Staff": 15000, "The Hero's Sword": 25000, "+3 Weapon": 18000})
+		
+		self.range_list = [self.c, self.b, self.b1, self.b2, self.b3, self.b4]
+		
 	def print_bpower(self):
-		print(self.C)
-		print(self.B)
-		print(self.B1)
-		print(self.B2)
-		print(self.B3)
-		print(self.B4)
+		self.c.print_bp()
+		self.b.print_bp()
+		self.b1.print_bp()
+		self.b2.print_bp()
+		self.b3.print_bp()
+		self.b4.print_bp()
 		
 	def gen_gold(self, dif):
 		i = dif % 4
-		m_tier = self.range_list[dif]
-		self.H = int((m_tier *(3.5 * (i + 1.45)) + 65) * random.uniform(0.85, 1.15))
-		#print(str(self.H))
+		m_tier = self.range_list[int(dif/4)]
+		self.H = int((m_tier.bpower *(3.5 * (i + 1.45)) + 65) * random.uniform(0.85, 1.15))
 	
-	def gen_loot(self):
-		#m_tier = self.range_list[dif]
-		arr_num = len(self.c_loot)		
-		loot = self.c_loot[random.randint(0,(arr_num - 1))]
-		self.H = self.H - self.c_val[loot]
+	def gen_loot(self, dif):
+		m_tier = self.range_list[int(dif/4)]
+		arr_num = len(m_tier.loot)
+		loot = m_tier.loot[random.randint(0,(arr_num - 1))]	
+		self.H = self.H - m_tier.loot_val[loot]
 		return(loot)
 	
 	def gen_horde(self):
@@ -63,21 +52,21 @@ class Horde_Maker():
 			try:
 				d = input("(Enter 0 to exit)Please enter the difficulty of the dungeon on a scale of 1-20: ")
 				dif = int(d)
+				m_tier = self.range_list[int(dif/4)]
 				
 				if m != 0:
-					loot_amount = random.randint(1, 4)
+					loot_amount = random.randint(1, 2)
 					loot_total = ""
 					self.gen_gold(dif)
 						
 					while(loot_amount != 0):
-						loot = self.gen_loot()
+						loot = self.gen_loot(dif)
 						loot_total += loot + ", "
 						loot_amount = loot_amount - 1
 						
 						
 					print(str(self.H) + " Gold pieces")
 					print(loot_total)
-					#print(self.c_val[loot])
 					
 				else:
 					print("Thank you for using my Horde Generator Program! ")
@@ -87,6 +76,15 @@ class Horde_Maker():
 				
 			except IndexError:
 				print("The number you entered was too large. Enter an integer between 1-20. ")
+				
+				
+	class Power():
+		def __init__(self, bpower, loot = [], loot_val = {}):
+			self.bpower = bpower
+			self.loot = loot
+			self.loot_val = loot_val
+		def print_bp(self):
+			print(str(self.bpower))
 
 horde1 = Horde_Maker()
 horde1.gen_bpower()
